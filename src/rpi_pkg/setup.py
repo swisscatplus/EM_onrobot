4a5ee937a275a4de4decf8f852d7e62ee270f568
Yanniscod <yannis.coderey@epfl.ch>
2024-05-29 14:40:16 +0000

    updates

--- src/rpi_pkg/setup.py
+++ src/rpi_pkg/setup.py
@@ -7,12 +7,13 @@ package_name = 'rpi_pkg'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, f'{package_name}.submodules'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
 	(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
