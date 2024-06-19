b1542dd86118b1b7f19a968868a0a1a2bca64376
Yanniscod <yannis.coderey@epfl.ch>
2024-06-19 12:14:05 +0000

    retrying focal

--- calibration/take_images.py
+++ calibration/take_images.py
@@ -6,9 +6,9 @@ import time
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
 picam2.start()
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 2.5}) 
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 5.0}) 
 
-num = 10
+num = 110
 
 while True:
     frame = picam2.capture_array()
