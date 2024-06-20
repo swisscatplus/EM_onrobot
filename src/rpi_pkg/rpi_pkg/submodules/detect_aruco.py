f1cebeebddf30e18bd5e697142a170f8f4e0bc08
Yanniscod <102654647+Yanniscod@users.noreply.github.com>
2024-06-20 11:17:42 +0200

    Remeasuring conv factor with calibration

--- src/rpi_pkg/rpi_pkg/submodules/detect_aruco.py
+++ src/rpi_pkg/rpi_pkg/submodules/detect_aruco.py
@@ -72,7 +72,7 @@ class CameraVisionStation:
         self.logger.setLevel(logging.DEBUG)
         ch = logging.StreamHandler()
 
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
