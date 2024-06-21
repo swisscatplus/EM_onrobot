1dcd494554910e6b5c7a11265e07bb063bf23ef6
Yanniscod <102654647+Yanniscod@users.noreply.github.com>
2024-06-21 10:29:47 +0200

    Cleaning heavy pkl files

---
+++ install/rpi_pkg/share/rpi_pkg/launch/rpi.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'rpi_pkg'  
+
+    imu_config = os.path.join(
+        get_package_share_directory(pkg_name),
+        'config',
+        'bno055_params.yaml'
+        )
+
+    rpi_node = Node(
+            package = 'rpi_pkg',
+            executable = 'rpi_motors',
+            output = 'screen',
+        )
+    cam_node = Node(
+            package = 'rpi_pkg',
+            executable = 'rpi_cam',
+            output = 'screen',
+        ) 
+    imu_node=Node(
+        package = 'bno055',
+        executable = 'bno055',
+        parameters = [imu_config]
+    )
+
+    return LaunchDescription([
+        rpi_node,
+        cam_node,
+        imu_node
+    ])
