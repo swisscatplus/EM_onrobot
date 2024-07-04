d6babb3cca13773d4b448809f0ac937d3cb6af06
yannis.coderey@epfl.ch <102654647+Yanniscod@users.noreply.github.com>
2024-07-04 14:37:14 +0200

    adding updated shell scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -it --network host --privileged -v /dev/:/dev/ -v /run/udev:/run/udev username/img_name:tag
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: ./run.sh namespace, running without namespace"
+  docker run -it --network host --privileged -v /dev/:/dev/ -v /run/udev:/run/udev  yanniscod/robot:og
+else
+  echo "Running with namespace $1"
+  docker run -it --network host --privileged -v /dev/:/dev/ -v /run/udev:/run/udev -e namespace=$1 yanniscod/robot:og
+fi
