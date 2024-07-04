400f37e1d8ee919a2d85636f6dc195efbb2680c2
yannis.coderey@epfl.ch <102654647+Yanniscod@users.noreply.github.com>
2024-07-04 14:37:57 +0200

    adding updated shell scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # builds with --no-cache to update the git informations, in case new infos were pushed (otherwise takes from docker cache)
 
-docker build . --no-cache -t username/img_name:tag
+docker build --no-cache --build-arg GIT_USER=Yanniscod --build-arg GIT_TOKEN=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 -t username/img_name:tag Desktop/SwissCat-on_robot
