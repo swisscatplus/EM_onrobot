0571519d901646dc6d33447027746c0a3b240dc5
Yanniscod <102654647+Yanniscod@users.noreply.github.com>
2024-07-12 15:29:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # builds with --no-cache to update the git informations, in case new infos were pushed (otherwise takes from docker cache)
 
-docker build --no-cache --build-arg GIT_USER= --build-arg GIT_TOKEN= -t username/img_name:tag Desktop/SwissCat-on_robot
+docker build --no-cache --build-arg GIT_USER= --build-arg GIT_TOKEN= -t username/img_name:tag Desktop/EM_onrobot
