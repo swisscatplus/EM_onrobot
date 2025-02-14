57419c5dd826d7d046ca68c0b9ff907713655e32
Edy Mariano <edy.mariano1@gmail.com>
2025-02-14 14:45:02 +0100

    add ros_domain to 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 # Source le workspace local
 source /ros2_ws/install/setup.bash
 
+export ROS_DOMAIN_ID=10
+
 # Exécuter la commande passée en arguments (CMD)
 exec "$@"
